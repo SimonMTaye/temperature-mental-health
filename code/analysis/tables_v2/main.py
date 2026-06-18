b4b91bb998a320702f442ea89b6ab0cf9f1e387c
Simon Taye <mulat.simon@gmail.com>
2026-06-18 11:56:03 -0400

    Add logging to table runner

--- code/analysis/tables_v2/main.py
+++ code/analysis/tables_v2/main.py
@@ -8,17 +8,26 @@ from analysis.tables_v2.table_e_job_loss_window_robustness import make_table as 
 from analysis.tables_v2.table_f_job_loss_controls import make_table as table_f
 from analysis.tables_v2.table_g_fuel_shock_alt_definitions import make_table as table_g
 from analysis.tables_v2.table_h_sumstats import make_table as table_h
+from library.log import log
 
 
 def main() -> None:
     table_a()
+    log("Table A completed")
     table_b()
+    log("Table B completed")
     table_c()
+    log("Table C completed")
     table_d()
+    log("Table D completed")
     table_e()
+    log("Table E completed")
     table_f()
+    log("Table F completed")
     table_g()
+    log("Table G completed")
     table_h()
+    log("Table H completed")
 
 
 if __name__ == "__main__":
