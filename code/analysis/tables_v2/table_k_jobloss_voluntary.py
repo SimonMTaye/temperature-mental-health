c963afcb13d324d65b6af6314aeabc618673c055
Simon Taye <mulat.simon@gmail.com>
2026-06-19 11:16:50 -0400

    Breakdown job loss categories more

--- code/analysis/tables_v2/table_k_jobloss_voluntary.py
+++ code/analysis/tables_v2/table_k_jobloss_voluntary.py
@@ -12,19 +12,28 @@ from dataclasses import replace
 
 def make_table() -> None:
     jobloss_df = analysis_df.copy().assign(
+        kabupaten_full_code=lambda df: df["gadm_fullcode"].astype("int64") // 1000,
         job_loss_involuntary=lambda df: (
-            df[JOB_LOSS_MAIN] * df["involuntary_loss_5y"]
+            df[JOB_LOSS_MAIN] * df["job_loss_involuntary"]
         ).astype("Int32"),
-        job_loss_voluntary=lambda df: 1 - df.job_loss_involuntary,
+        job_loss_voluntary=lambda df: (
+            df[JOB_LOSS_MAIN] * (df["job_loss_family"])
+        ).astype("Int32"),
+        job_loss_sick=lambda df: (
+            df[JOB_LOSS_MAIN]
+            * (1 - df["job_loss_family"])
+            * (1 - df["job_loss_involuntary"])
+        ).astype("Int32"),
     )
     rename_dict = {
         JOB_LOSS_MAIN: "Job Loss",
         "job_loss_involuntary": "Job Loss",
         "job_loss_voluntary": "Job Loss",
+        "job_loss_sick": "Job Loss",
         MAIN_TEMP_MEASURE: "Heat",
     }
     jobloss_specs = [
-        jobloss,
+        replace(jobloss, df=jobloss_df),
         replace(
             update_formula_search_replace(
                 jobloss, JOB_LOSS_MAIN, "job_loss_involuntary"
@@ -35,6 +44,19 @@ def make_table() -> None:
             update_formula_search_replace(jobloss, JOB_LOSS_MAIN, "job_loss_voluntary"),
             df=jobloss_df,
         ),
+        replace(
+            update_formula_search_replace(jobloss, JOB_LOSS_MAIN, "job_loss_sick"),
+            df=jobloss_df,
+        ),
+    ]
+    jobloss_treated_people = [
+        jobloss_df[job_loss_var].eq(1).sum()
+        for job_loss_var in [
+            JOB_LOSS_MAIN,
+            "job_loss_involuntary",
+            "job_loss_voluntary",
+            "job_loss_sick",
+        ]
     ]
     table = make_regression_table(
         jobloss_specs,
@@ -43,8 +65,10 @@ def make_table() -> None:
         titles=[
             "Job Loss",
             r"\shortstack{Job Loss\\Involuntary}",
-            r"\shortstack{Job Loss\\Voluntary}",
+            r"\shortstack{Job Loss\\Family}",
+            r"\shortstack{Job Loss\\Sickness}",
         ],
+        custom_model_stats={"People with job loss": jobloss_treated_people},
     )
     render_table_to_latex(
         table,
